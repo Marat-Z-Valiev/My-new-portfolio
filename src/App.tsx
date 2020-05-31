b6869d9bed948b46521d628ebd662fb439069259
Marat Valiev <mz.valiev@gmail.com>
2020-05-30 22:20:10 -0400

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {createGlobalStyle} from "styled-components";
+import NavBar from "./Components/NavBar/NavBar";
+
+const App = () => {
+	const GlobalStyle = createGlobalStyle`
+	body {
+	font-family: "Baloo Chettan 2", cursive;
+	background-color: #0c6cb4;
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+	}
+
+	.error-message{
+		text-align: center;
+		color: #ffffff;
+		font-size: 1.3rem;
+	}
+	`;
+
+	return (
+		<>
+			<GlobalStyle>
+				<NavBar></NavBar>
+			</GlobalStyle>
+		</>
+	);
+};
+
+export default App;
---
+++ src/Components/NavBar/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NavBar = () => {
+	return (
+		<ul>
+			<li>Projects</li>
+			<li>Skill Set</li>
+			<li>Contact</li>
+		</ul>
+	);
+};
+
+export default NavBar;
