7318135f0b5dc2d3df3426801332621e29c53fd0
Marat Valiev <mz.valiev@gmail.com>
2020-07-14 21:34:18 -0400

    Add new project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,7 +2,7 @@ const projects = {
 	project1: {
 		name: "Movies API",
 		description:
-			"The project built using ReactJS, TypeScript, React Router, Redux, Jest. Utilizing  (TMDB API). Fully responsive and accessible.",
+			"The porject is utilizing  (TMDB API) to display informarion about movies, actors, popular movies and also search for movies. Built using ReactJS, TypeScript, React Router, Redux, Jest. Fully responsive and accessible",
 		image: "https://i.ibb.co/JQr8Zmp/movie-api-image-resized.jpg",
 		websiteLink: "https://movies-api-marat.netlify.app/",
 		githubLink: "https://github.com/Marat-Z-Valiev/movies-api-tmdb",
@@ -14,20 +14,17 @@ const projects = {
 		],
 	},
 	project2: {
-		name: "Movies API",
+		name: "StoryBooks",
 		description:
-			"The project built using ReactJS, TypeScript, API, React Router, Redux, Jest",
-		image: "https://i.ibb.co/TB1ZXJL/Pic-App-Apple-i-Mac-cropped.jpg",
-		websiteLink: "https://marat-z-valiev.github.io/Random_quote_machine",
-		globeIcon: "fa fa-globe fa-2x",
-		githubLink: "https://github.com/Marat-Z-Valiev/Random_quote_machine",
-		githubIcon: "fa fa-github fa-2x",
+			"StoryBooks allows you to login into the app using Google account. Users can create, edit and delete stories. The project built using NodeJS, Express, MongoDB, Passport js for Google Auth, Handlebars framework",
+		image: "https://i.ibb.co/m0dsKts/Story-Books-project.png",
+		websiteLink: "https://storybooks-nodejs-app.herokuapp.com/",
+		githubLink: "https://github.com/Marat-Z-Valiev/nodejs-app",
 		backgroundClass: "blue",
 		position: "right",
 		technologies: [
-			"https://i.ibb.co/qYDMK54/React-js-logo.png",
-			"https://i.ibb.co/QCN8KZ9/redux.png",
-			"https://i.ibb.co/Ct9gHzc/typescript.png",
+			"https://i.ibb.co/WHxbkfj/mongodb-logo.png",
+			"https://i.ibb.co/52XxdJp/603784-nodejs-512x512.png",
 		],
 	},
 	project3: {
