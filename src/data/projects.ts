ed93d1e71317c8c1a16bfeda09688ed8e3c3faa3
Marat Valiev <maratvaliev@Marats-MacBook-Pro-2.local>
2023-07-10 10:35:26 -0400

    fix typo

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,64 +1,63 @@
 const projects = {
-	project1: {
-		name: "Movies API",
-		description:
-			"The project is utilizing  (TMDB API) to display informarion about movies, actors, popular movies and also search for movies. Built using ReactJS, TypeScript, React Router, Redux, Jest. Fully responsive and accessible",
-		image: "https://i.ibb.co/JQr8Zmp/movie-api-image-resized.jpg",
-		websiteLink: "https://movies-api-marat.netlify.app/",
-		githubLink: "https://github.com/Marat-Z-Valiev/movies-api-tmdb",
-		backgroundClass: "white",
-		technologies: [
-			"https://i.ibb.co/qYDMK54/React-js-logo.png",
-			"https://i.ibb.co/QCN8KZ9/redux.png",
-			"https://i.ibb.co/Ct9gHzc/typescript.png",
-		],
-	},
-	project2: {
-		name: "Expenses tracker",
-		description:
-			"Simple expenses tracker app. Built using React, Firebase, Typescript",
-		image: "https://i.ibb.co/JHTjcNK/Screenshot-2023-06-20-at-11-03-35-PM.png",
-		websiteLink: "https://manage-expenses-app-mv.netlify.app/",
-		githubLink: "https://github.com/Marat-Z-Valiev/expenses-app",
-		backgroundClass: "blue",
-		position: "right",
-		technologies: [
-			"https://cdn.freebiesupply.com/logos/thumbs/2x/firebase-1-logo.png",
-			"https://i.ibb.co/qYDMK54/React-js-logo.png",
-			"https://i.ibb.co/Ct9gHzc/typescript.png"
-		],
-	},
-	project3: {
-		name: "Landing page project",
-		description:
-			"I built this landing page as part of a challenge on Frontend Mentor website. The website was created using best practices of web accessibility and responsive design.",
-		image: "https://i.ibb.co/92cWw0Y/desktop-preview.jpg",
-		websiteLink: "https://landing-page-mv.netlify.app/",
-		githubLink: "https://github.com/Marat-Z-Valiev/landing-page-project",
-		backgroundClass: "white",
-		technologies: [
-			"https://i.ibb.co/q98SPqF/288-Sass-512.png",
-			"https://i.ibb.co/b5kc6P6/html.png",
-			"https://i.ibb.co/Z2fvk3S/css.jpg",
-			"https://i.ibb.co/0DzXyBn/js.png",
-		],
-	},
-	project4: {
-		name: "Recipes list",
-		description:
-			"Recipes list project using Vue, Vite, MongoDB, TypeScript",
-		image: "https://i.ibb.co/RYCxd1T/Screenshot-2023-07-03-at-9-16-49-PM.png",
-		websiteLink: "https://recipes-vue-app-mv.netlify.app/",
-		githubLink: "https://github.com/Marat-Z-Valiev/recipes-app-vue",
-		backgroundClass: "blue",
-		position: "right",
-		technologies: [
-			"https://i.ibb.co/mN3ppyM/1184px-Vue-js-Logo-2-svg.png",
-			"https://i.ibb.co/W5q7Kny/logo-with-shadow.png",
-			"https://i.ibb.co/WHxbkfj/mongodb-logo.png",
-			"https://i.ibb.co/Ct9gHzc/typescript.png",
-		],
-	},
+  project1: {
+    name: "Movies API",
+    description:
+      "The project is utilizing  (TMDB API) to display information about movies, actors, popular movies and also search for movies. Built using ReactJS, TypeScript, React Router, Redux, Jest. Fully responsive and accessible",
+    image: "https://i.ibb.co/JQr8Zmp/movie-api-image-resized.jpg",
+    websiteLink: "https://movies-api-marat.netlify.app/",
+    githubLink: "https://github.com/Marat-Z-Valiev/movies-api-tmdb",
+    backgroundClass: "white",
+    technologies: [
+      "https://i.ibb.co/qYDMK54/React-js-logo.png",
+      "https://i.ibb.co/QCN8KZ9/redux.png",
+      "https://i.ibb.co/Ct9gHzc/typescript.png",
+    ],
+  },
+  project2: {
+    name: "Expenses tracker",
+    description:
+      "Simple expenses tracker app. Built using React, Firebase, Typescript",
+    image: "https://i.ibb.co/JHTjcNK/Screenshot-2023-06-20-at-11-03-35-PM.png",
+    websiteLink: "https://manage-expenses-app-mv.netlify.app/",
+    githubLink: "https://github.com/Marat-Z-Valiev/expenses-app",
+    backgroundClass: "blue",
+    position: "right",
+    technologies: [
+      "https://cdn.freebiesupply.com/logos/thumbs/2x/firebase-1-logo.png",
+      "https://i.ibb.co/qYDMK54/React-js-logo.png",
+      "https://i.ibb.co/Ct9gHzc/typescript.png",
+    ],
+  },
+  project3: {
+    name: "Landing page project",
+    description:
+      "I built this landing page as part of a challenge on Frontend Mentor website. The website was created using best practices of web accessibility and responsive design.",
+    image: "https://i.ibb.co/92cWw0Y/desktop-preview.jpg",
+    websiteLink: "https://landing-page-mv.netlify.app/",
+    githubLink: "https://github.com/Marat-Z-Valiev/landing-page-project",
+    backgroundClass: "white",
+    technologies: [
+      "https://i.ibb.co/q98SPqF/288-Sass-512.png",
+      "https://i.ibb.co/b5kc6P6/html.png",
+      "https://i.ibb.co/Z2fvk3S/css.jpg",
+      "https://i.ibb.co/0DzXyBn/js.png",
+    ],
+  },
+  project4: {
+    name: "Recipes list",
+    description: "Recipes list project using Vue, Vite, MongoDB, TypeScript",
+    image: "https://i.ibb.co/RYCxd1T/Screenshot-2023-07-03-at-9-16-49-PM.png",
+    websiteLink: "https://recipes-vue-app-mv.netlify.app/",
+    githubLink: "https://github.com/Marat-Z-Valiev/recipes-app-vue",
+    backgroundClass: "blue",
+    position: "right",
+    technologies: [
+      "https://i.ibb.co/mN3ppyM/1184px-Vue-js-Logo-2-svg.png",
+      "https://i.ibb.co/W5q7Kny/logo-with-shadow.png",
+      "https://i.ibb.co/WHxbkfj/mongodb-logo.png",
+      "https://i.ibb.co/Ct9gHzc/typescript.png",
+    ],
+  },
 };
 
 export default projects;
